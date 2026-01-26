c085849302e047e5731d99d96492cdacb0e67719
Santos Flores <santosflores@gmail.com>
2026-01-26 12:01:59 -0800

    chore(server): update server port to 8000

--- backend/server.ts
+++ backend/server.ts
@@ -19,7 +19,7 @@ registerHomeRoute(fastify, runner);
 
 const start = async () => {
     try {
-        await fastify.listen({ port: 3000 });
+        await fastify.listen({ port: 8000 });
     } catch (err) {
         fastify.log.error(err);
         process.exit(1);
