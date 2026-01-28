8043381e02b5082fd0badfd31d8bc41ea49301e0
Santos Flores <santosflores@gmail.com>
2026-01-27 17:03:06 -0800

    chore: remove debug scripts

--- backend/inspect_service.ts
+++
@@ -1,9 +0,0 @@
-
-import { InMemoryRunner } from '@google/adk';
-import { orchestratorAgent } from './agents/agent';
-
-const runner = new InMemoryRunner({
-    agent: orchestratorAgent,
-});
-
-console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(runner.sessionService)));
